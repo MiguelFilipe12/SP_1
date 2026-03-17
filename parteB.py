67a91237155cf38bf1b97953c15617d8f84c12b0
laradinis <laraldinis@gmail.com>
2026-03-17 17:05:00 +0000

    Update parteB.py
    :P

--- parteB.py
+++ parteB.py
@@ -1,24 +1,24 @@
-from os import urandom
+import os
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 
 
 def aes_encrypt_file(input_file, key):
 
 
-    with open(input_file, 'rb') as f:
-        text = f.read()
+    with open(input_file, 'rb') as f:                             #rb = read binary
+        text = f.read()                                           #passa a texto
 
 
-    nonce = urandom(16)
+    nonce = urandom(16)                                           #numero aleatório de 16 bytes
 
-    cipher = Cipher(algorithms.AES(key), modes.CTR(nonce))
-    encryptor = cipher.encryptor()
+    cipher = Cipher(algorithms.AES(key), modes.CTR(nonce))        #modo de cifrar (o algorítmo: (AES) e o modo: (CTR))
+    encryptor = cipher.encryptor()                                #??????????????
 
-    ciphertext = encryptor.update(text) + encryptor.finalize()
+    ciphertext = encryptor.update(text) + encryptor.finalize()    #encriptar o texto
 
-    return nonce, ciphertext
+    return nonce, ciphertext                                      #return texto encriptado
 
 
-nonce, ciphertext = aes_encrypt_file('file_8.txt', urandom(16))
+nonce, ciphertext = aes_encrypt_file('file_8.txt', urandom(32))    #teste
 
 print(f'\nCiphertext: {ciphertext.hex()}')
